33a8863cbbc420d90f119fbb83b6abbf7b507a6a
Dongdong Tian <seisman.info@gmail.com>
2021-06-23 17:43:07 -0400

    Bump dcw to 2.0.0 (#6)
    * Bump dcw to 2.0.0
    
    * MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.10.3, and conda-forge-pinning 2021.06.18.08.15.47
    
    * Update LICENSE
    
    * Update the name of the LICENSE file
    
    * MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.10.3, and conda-forge-pinning 2021.06.18.08.15.47
    
    * Rename the name of the LICENSE file in tests
    
    * Update build scripts
    
    * Reset build number to 0
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,5 +3,7 @@ set DATADIR="%LIBRARY_PREFIX%\share\dcw-gmt"
 if not exist %DATADIR% mkdir %DATADIR%
 
 xcopy %SRC_DIR%\*.txt %DATADIR%
-xcopy %SRC_DIR%\COPYING* %DATADIR%
+xcopy %SRC_DIR%\LICENSE %DATADIR%
+xcopy %SRC_DIR%\ChangeLog %DATADIR%
+xcopy %SRC_DIR%\README.md %DATADIR%
 xcopy %SRC_DIR%\*.nc %DATADIR% /s /e || exit 1
